1a6cc5ca8935ca038a43f8965f4c5ffae7865e0f
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2024-06-27 15:03:16 +0900

    Add SkillBase
    스킬 인터페이스

---
+++ Project_6/Assets/Scripts/P_ISkill.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface P_ISkill
+{
+    void SkillAction();
+
+}
---
+++ Project_6/Assets/Scripts/Skill/Sword/P_SwordQ.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_SwordQ : P_ISkill
+{
+    
+}
