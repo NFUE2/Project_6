cd82e8d3f5b2d84b99579cdf58e5cb47a6731a2d
birdham <139436177+birdham@users.noreply.github.com>
2024-06-28 15:11:23 +0900

    >>update player

--- Project_6/Assets/Jiwoon/Scripts/P_PlayerCondition.cs
+++ Project_6/Assets/Jiwoon/Scripts/P_PlayerCondition.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerCondition : MonoBehaviour
 {
+    public static PlayerCondition Instance { get; private set; } // 싱글톤 인스턴스
+
     public int maxHealth = 100; // 최대 체력
     private int currentHealth;
     private BoxCollider2D damagable;
 
     private void Awake()
     {
-        GetComponent<BoxCollider2D>();
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬이 변경되어도 오브젝트가 파괴되지 않도록 설정
+        }
+        else
+        {
+            Destroy(gameObject); // 이미 인스턴스가 존재하면 새로운 인스턴스를 파괴
+        }
+
+        damagable = GetComponent<BoxCollider2D>();
+        currentHealth = maxHealth; // 초기 체력을 최대 체력으로 설정
     }
 
     // 데미지를 받는 메서드
