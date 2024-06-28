3cdbfed2d04933261e77494448525d8ef344fe5c
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2024-06-28 14:31:30 +0900

    Update Script,Prefab
    -Skill

--- Project_6/Assets/Scripts/P_Skill/Gun/P_GunE.cs
+++ Project_6/Assets/Scripts/P_Skill/Gun/P_GunE.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class P_GunE : MonoBehaviour, P_ISkill
 {
-    Animator animator;
+    //Animator animator;
     Rigidbody2D rigidbody;
     public float rollingX;
 
@@ -12,19 +12,23 @@ public class P_GunE : MonoBehaviour, P_ISkill
 
     private void Awake()
     {
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     public void SkillAction()
     {
-        animator.SetTrigger("SkillE");
+        //animator.SetTrigger("SkillE");
         isRolling = true;
         rigidbody.velocity = new Vector2(rollingX,0);
+
+        Invoke("ExitRolling", 1.0f);
         //SkillEAction?.Invoke();
     }
 
     public void ExitRolling()
     {
         isRolling = false;
+        rigidbody.velocity = Vector2.zero;
     }
 }
