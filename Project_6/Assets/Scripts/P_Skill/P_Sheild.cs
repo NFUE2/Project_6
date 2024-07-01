e08a7aa38614c6df287346a33bee4bfa05b6a283
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2024-07-01 20:05:17 +0900

    fix prefab

--- Project_6/Assets/Scripts/P_Skill/P_Sheild.cs
+++ Project_6/Assets/Scripts/P_Skill/P_Sheild.cs
@@ -15,11 +15,12 @@ public class P_Sheild : MonoBehaviour,P_IDamagable
     public void TakeDamage(float damage)
     {
         amount -= damage;
+        Debug.Log(1);
         if(amount <= 0) DestroyObject();
     }
 
     private void DestroyObject()
     {
-        PhotonNetwork.Destroy(gameObject);
+        PhotonNetwork.Destroy(transform.parent.gameObject);
     }
 }
