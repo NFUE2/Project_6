69e48c889104059b3f81792df52af45861d48ba7
birdham <139436177+birdham@users.noreply.github.com>
2024-07-02 12:46:06 +0900

    >>fix arrow

--- Project_6/Assets/Jiwoon/Scripts/ArrowScript.cs
+++ Project_6/Assets/Jiwoon/Scripts/ArrowScript.cs
@@ -8,7 +8,6 @@ public class ArrowScript : MonoBehaviour
     private Camera mainCamera;
     public float damage;
     PhotonView pv;
-
     void Start()
     {
         mainCamera = Camera.main;
@@ -28,6 +27,7 @@ public class ArrowScript : MonoBehaviour
         // 화면 좌표를 이용하여 화면 밖에 있는지 확인
         if (screenPos.x < 0 || screenPos.x > Screen.width || screenPos.y < 0 || screenPos.y > Screen.height)
         {
+            //Destroy(gameObject); // 화면 밖으로 나갔을 때 총알 파괴
             DestroyObject();
         }
     }
