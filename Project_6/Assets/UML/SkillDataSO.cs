7c7106a39b4b365ed031b346d2a990c64a304b66
ErmaelStarius <ermael35@daum.net>
2024-07-05 15:12:39 +0900

    Update : UML
    UML 추가 사항 업데이트

---
+++ Project_6/Assets/UML/SkillDataSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkillDataSO", menuName = "Scriptable Object/SkillDataSO")]
+
+public class SkillDataSO : ObjectSO
+{
+    public float coolTime;
+
+    public float lastActionTime;
+
+    public bool isQuickAction;
+}
