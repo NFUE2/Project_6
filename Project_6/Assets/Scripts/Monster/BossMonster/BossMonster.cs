b38eb15b8e328c1995dfd5755d4dfd6d2ad697fd
twotop6357 <twotop6357@gmail.com>
2024-07-10 20:04:32 +0900

    Add BossTakeDamage

--- Project_6/Assets/Scripts/Monster/BossMonster/BossMonster.cs
+++ Project_6/Assets/Scripts/Monster/BossMonster/BossMonster.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
 
-public class BossMonster : MonoBehaviour
+public class BossMonster : MonoBehaviour, IDamagable
 {
     public float maxHp { get; set; }
+
+    private float currentHp;
     public float attackPower { get; set; }
     public float defensePower { get; set; }
     public float moveSpeed {  get; set; }
+
+    public void TakeDamage(float damage)
+    {
+        currentHp -= damage;
+        if (currentHp <= 0)
+        {
+            // »ç¸Á Ã³¸®
+        }
+    }
 }
