12edb603ad362872fe45e097fc8e48e381142125
twotop6357 <twotop6357@gmail.com>
2024-07-12 14:51:42 +0900

    Update GolemData

--- Project_6/Assets/Scripts/Monster/BossMonster/Golem/GolemBoss.cs
+++ Project_6/Assets/Scripts/Monster/BossMonster/Golem/GolemBoss.cs
@@ -1,15 +1,16 @@
 public class GolemBoss : BossMonster
 {
+    public CharacterDataSO golemData;
     // 데이터SO
     private void Start()
     {
         // 데이터SO에서 스탯 설정
         // 임시 데이터
-        maxHp = 10000;
+        maxHp = golemData.maxHP;
         currentHp = maxHp;
-        attackPower = 20;
-        defensePower = 20;
-        moveSpeed = 1;
+        attackPower = golemData.attackDamage;
+        defensePower = golemData.deffenceDamage;
+        moveSpeed = golemData.moveSpeed;
         // 임시 데이터
         hpBar.fillAmount = GetFillAmountHP();
     }
