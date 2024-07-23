c31d3b53c5e6e3cc4694def945b779de445f6ab4
birdham <139436177+birdham@users.noreply.github.com>
2024-07-23 20:28:03 +0900

    >>fix dagger
    >>fix dagger

--- Project_6/Assets/Jiwoon/Scripts/Jobs/Dagger/DaggerPlayer.cs
+++ Project_6/Assets/Jiwoon/Scripts/Jobs/Dagger/DaggerPlayer.cs
@@ -1,6 +1,7 @@
 using Photon.Pun;
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class DaggerPlayer : MeleePlayerBase
 {
@@ -18,8 +19,10 @@ public class DaggerPlayer : MeleePlayerBase
 
     public override void Attack()
     {
-        base.Attack();
-        CheckAndIncreaseStack();
+        if (isAttacking) return;  // 공격 중이 아닌 경우에만 공격
+        isAttacking = true;
+        animator.SetTrigger("IsAttack");
+        StartCoroutine(PerformAttackWithStackCheck());
     }
 
     public override void UseSkillQ()
@@ -32,17 +35,25 @@ public class DaggerPlayer : MeleePlayerBase
         stackSkill.UseSkill();
     }
 
-    private void CheckAndIncreaseStack()
+    private IEnumerator PerformAttackWithStackCheck()
     {
+        //공격이 넣어지고 데미지가 들어갔을때만 스택이 오른다.
+        yield return new WaitForSeconds(0.1f); // 공격 애니메이션의 타이밍에 맞춰 대기
+
         Vector2 attackPosition = CalculateAttackPosition();
         Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackPosition, attackSize, 0, enemyLayer);
 
         foreach (Collider2D enemy in hitEnemies)
         {
-            if (enemy.GetComponent<IDamagable>() != null)
+            var damageable = enemy.GetComponent<IDamagable>();
+            if (damageable != null)
             {
-                stackSkill.IncreaseStack();
+                damageable.TakeDamage(attackDamage);
+                stackSkill.IncreaseStack(); // 적이 데미지를 입었을 때만 스택 증가
             }
         }
+
+        yield return new WaitForSeconds(attackCooldown - 0.1f); // 남은 쿨타임 대기
+        isAttacking = false;
     }
 }
