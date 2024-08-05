6e7a8eb2f282b7bf9661f4526b052fb242c5431f
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2024-08-05 15:35:38 +0900

    Update : Resource

--- Project_6/Assets/Scripts/Player/PlayerCondition.cs
+++ Project_6/Assets/Scripts/Player/PlayerCondition.cs
@@ -53,7 +53,6 @@ public class PlayerCondition : MonoBehaviourPun, IDamagable, IKnockBackable
         {
             UpdateHealthBar();
         }
-        
 
         if (currentHealth <= 0)
         {
