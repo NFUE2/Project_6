eaca908cf427b1f0be742c93e884e4c2db7e365c
twotop6357 <twotop6357@gmail.com>
2024-08-08 12:10:02 +0900

    Update LobbyUI

--- Project_6/Assets/Scripts/UI/Intro Scene/IntroPopup.cs
+++ Project_6/Assets/Scripts/UI/Intro Scene/IntroPopup.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class IntroPopup : UIBase
 {
     public Button[] buttons;
+    public GameObject networkPanel, panelExitButton;
+    public TextMeshProUGUI infoText;
 
-    private void SendButtons()
+    private void SetNetworkManager()
     {
+        NetworkManager.Instance.networkPanel = networkPanel;
+        NetworkManager.Instance.panelExitButton = panelExitButton;
+        NetworkManager.Instance.infoText = infoText;
         NetworkManager.Instance.buttons = buttons;
     }
 
@@ -17,7 +23,7 @@ public class IntroPopup : UIBase
         switch(str)
         {
             case "Play":
-                SendButtons();
+                SetNetworkManager();
                 NetworkManager.Instance.OnClickEnterServer();
                 UIManager.Instance.Show<LobbyUI>();
                 break;
