240185fd91f537f3e68e913a32e8c836badc7ab4
twotop6357 <twotop6357@gmail.com>
2024-08-08 11:54:34 +0900

    Fix LobbyUI_NF

--- Project_6/Assets/Scripts/UI/Intro Scene/IntroPopup.cs
+++ Project_6/Assets/Scripts/UI/Intro Scene/IntroPopup.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntroPopup : UIBase
 {
+    public Button[] buttons;
+
+    private void SendButtons()
+    {
+        NetworkManager.Instance.buttons = buttons;
+    }
+
     public void OnClickButton(string str)
     {
         switch(str)
         {
             case "Play":
+                SendButtons();
+                NetworkManager.Instance.OnClickEnterServer();
                 UIManager.Instance.Show<LobbyUI>();
                 break;
             case "Option":
