bc5ad7100af9d2786b4479c3a99a5cfa3a84a67b
birdham <139436177+birdham@users.noreply.github.com>
2024-08-16 12:48:16 +0900

    >>player

--- Project_6/Assets/Scripts/Player/Jobs/Dagger/DaggerPlayer.cs
+++ Project_6/Assets/Scripts/Player/Jobs/Dagger/DaggerPlayer.cs
@@ -1,5 +1,4 @@
 using Photon.Pun;
-using System.Collections;
 using UnityEngine;
 
 public class DaggerPlayer : MeleePlayerBase
@@ -12,45 +11,14 @@ public class DaggerPlayer : MeleePlayerBase
 
     private void Start()
     {
-        //dashSkill.SetCooldownText(qCooldownText);
-        //stackSkill.SetCooldownText(eCooldownText);
         dashSkill.SetCooldownImage(qCooldownImage);
         stackSkill.SetCooldownImage(eCooldownImage);
     }
 
     public override void Attack()
     {
-        // 기본 공격 로직 실행 (상속된 Attack 메서드 사용)
         base.Attack();
-
-        // 스택 증가 로직 추가
-        StartCoroutine(StackIncreaseCheck());
-    }
-
-    private IEnumerator StackIncreaseCheck()
-    {
-        // 공격이 끝날 때까지 기다림
-        yield return new WaitForSeconds(playerData.attackTime);
-
-        // 공격 범위 내 적을 다시 확인하고 스택 증가 처리
-        Vector2 attackPosition = CalculateAttackPosition();
-        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackPosition, attackSize, 0, enemyLayer);
-
-        bool hasIncreasedStack = false;
-
-        foreach (Collider2D enemy in hitEnemies)
-        {
-            var damagable = enemy.GetComponent<IDamagable>();
-            if (damagable != null)
-            {
-                // 첫 번째로 감지된 적에 대해 스택을 증가시킴
-                if (!hasIncreasedStack)
-                {
-                    stackSkill.IncreaseStack();
-                    hasIncreasedStack = true;
-                }
-            }
-        }
+        StartCoroutine(stackSkill.StackIncreaseCheck(playerData.attackTime));
     }
 
     public override void UseSkillQ()
--- Project_6/Assets/Scripts/Player/Jobs/Pistol/PistolPlayer.cs
+++ Project_6/Assets/Scripts/Player/Jobs/Pistol/PistolPlayer.cs
@@ -60,18 +60,18 @@ public class PistolPlayer : RangedPlayerBase
             if (currentAttackTime < playerData.attackTime)
             {
                 currentAttackTime += Time.deltaTime;
-                if (cooldownBar != null)
+                if (AttackcooldownBar != null)
                 {
-                    cooldownBar.fillAmount = currentAttackTime / playerData.attackTime;
+                    AttackcooldownBar.fillAmount = currentAttackTime / playerData.attackTime;
                 }
             }
         }
         else
         {
             // 장전 중에는 공격 바가 차지 않도록 설정
-            if (cooldownBar != null)
+            if (AttackcooldownBar != null)
             {
-                cooldownBar.fillAmount = 0f;
+                AttackcooldownBar.fillAmount = 0f;
             }
         }
     }
